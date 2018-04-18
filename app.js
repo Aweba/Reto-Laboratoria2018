d322d57acc22ef3495f6d47aeaa5cfb2c315e891
Aweba <fiorella.effio@unmsm.edu.pe>
2018-04-18 17:46:46 -0500

    Primera prueba

---
+++ app.js
@@ -0,0 +1,113 @@
+//Declaramos variables de las respuestas
+var r1 = '';
+var r2 = '';
+var r3 = '';
+var r4 = '';
+var r = 0;
+
+
+function enter()
+  {
+    welcome.style.fontSize = "25pt";
+    welcome.style.color = "white";
+document.getElementById("name").innerHTML = document.getElementById("firstname").value;
+    document.getElementById("ask").innerHTML = "¿Deseas jugar?";
+    document.getElementById("welcome").style.display = "block";
+ document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "block";
+  }
+function play()
+{
+  var answer=document.getElementsByName("answer0")[0].checked;
+  if(answer===false)
+  {
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("welcome").style.display = "none";
+    ask.style.fontSize = "30pt";
+    document.getElementById("ask").innerHTML =":(";
+  }
+  if(answer===true)
+  {
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("ask").innerHTML ="Primera pregunta:";
+    document.getElementById("section3").style.display = "block";
+  }
+}
+function pregunta1()
+{
+  var answer=document.getElementsByName("answer1")[0].checked;
+  document.getElementById("ask").innerHTML ="Segunda pregunta:";
+  document.getElementById("section3").style.display = "none";
+  document.getElementById("section4").style.display = "block";
+  if(answer===false)
+  {
+    r1 = "Beber gaseosa";
+    r=r+2;
+  }
+  if(answer===true)
+  {
+     r1 = "Beber agua";
+    r=r+1;
+  }
+  return r1;
+}
+
+function pregunta2()
+{
+  var answer=document.getElementsByName("answer2")[0].checked;
+  document.getElementById("ask").innerHTML ="Tercera pregunta:";
+  document.getElementById("section4").style.display = "none";
+  document.getElementById("section5").style.display = "block";
+  if(answer===false)
+  {
+    r2 = "Ensaladas/Pollo a la plancha";
+    r=r+2;
+  }
+  if(answer===true)
+  {
+     r2 = "Papas fritas/Pizza";
+     r=r+1;
+  }
+}
+
+function pregunta3()
+{
+  var answer=document.getElementsByName("answer3")[0].checked;
+  document.getElementById("section5").style.display = "none";
+  document.getElementById("section6").style.display = "block";
+
+  if(answer===false)
+  {
+    r3 = "Ver televisión";
+    r=r+2;
+  }
+  if(answer===true)
+  {
+   r3 = "Hacer ejercicios";
+   r=r+1;
+  }
+}
+
+function pregunta4()
+{
+  var answer=document.getElementsByName("answer4")[0].checked;
+  document.getElementById("section6").style.display = "none";
+
+  if(answer===false)
+  {
+    r4 = "3-5 horas";
+    r=r+2;
+  }
+  if(answer===true)
+  {
+    r4 = "6-8 horas";
+    r=r+1;
+  }
+  alert(r);
+  if (r>0 && r<8) {
+    document.getElementById("ask").innerHTML ="Resultado: \nFelicitaciones! Eres una persona muy saludable.";
+  } else {
+    document.getElementById("ask").innerHTML ="Resultado: \nCuidado! Tu salud puede estar en riesgo, trata de llevar una vida saludable.";
+  }
+}
